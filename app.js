082fe23f50f6ec0e19140e6a4dffc26c2eb0180b
Jigyasa Upadhyay <jigyasa.upadhyay@active.ai>
2022-01-18 17:15:17 +0530

    Names

--- app.js
+++ app.js
@@ -2,12 +2,9 @@
 
 //CommonJS - every file is a module
 //Modules - Encapsulated code (only share minimum)
-const john = 'john'
-const peter = 'peter'
-const sayHi =(name) => {
-console.log(`hello ${name}`)
-}
+const names = require('./names')
+const sayHi = require('./utils')
 
 sayHi('jigyasa')
-sayHi(peter);
-sayHi(john)
+sayHi(names.peter);
+sayHi(names.john)
